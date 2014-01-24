d02aea8823f47f77bc1b7b4c64ee7e503891b0e4
Keith Rochford <keith.rochford@gmail.com>
2014-01-24 14:11:47 +0000

    removed cluster nodes configuration

--- rabbitmq/chef/cookbooks/rabbitmq/recipes/default.rb
+++ rabbitmq/chef/cookbooks/rabbitmq/recipes/default.rb
@@ -165,23 +165,23 @@ else
 end
 
 if node['rabbitmq']['cluster'] && (node['rabbitmq']['erlang_cookie'] != existing_erlang_key)
-  # get ip addresses - Barclamp proposal needs to be coded and not hard coded
-   service_name = node[:rabbitmq][:config][:environment]
-   proposal_name = service_name.split('-')
-   bcproposal = "bc-rabbitmq-"+proposal_name[2]
-   getrmip_db = data_bag_item('crowbar', bcproposal)
-   rmcont1 = getrmip_db["deployment"]["rabbitmq"]["elements"]["rabbitmq"][0]
-   rmcont2 = getrmip_db["deployment"]["rabbitmq"]["elements"]["rabbitmq"][1]
-   rmcont3 = getrmip_db["deployment"]["rabbitmq"]["elements"]["rabbitmq"][2]
-   cluster_nodes = Array.new
-   rmcont1hostname = rmcont1.split('.')
-   rmcont2hostname = rmcont2.split('.')
-   rmcont3hostname = rmcont3.split('.')
-   cluster_nodes << "rabbit@"+rmcont1hostname[0]
-   cluster_nodes << "rabbit@"+rmcont2hostname[0]
-   cluster_nodes << "rabbit@"+rmcont3hostname[0]
-   node.default['rabbitmq']['cluster_disk_nodes'] = cluster_nodes
-  #End of cluster cluster address config
+  ## get ip addresses - Barclamp proposal needs to be coded and not hard coded
+   #service_name = node[:rabbitmq][:config][:environment]
+   #proposal_name = service_name.split('-')
+   #bcproposal = "bc-rabbitmq-"+proposal_name[2]
+   #getrmip_db = data_bag_item('crowbar', bcproposal)
+   #rmcont1 = getrmip_db["deployment"]["rabbitmq"]["elements"]["rabbitmq"][0]
+   #rmcont2 = getrmip_db["deployment"]["rabbitmq"]["elements"]["rabbitmq"][1]
+   #rmcont3 = getrmip_db["deployment"]["rabbitmq"]["elements"]["rabbitmq"][2]
+   #cluster_nodes = Array.new
+   #rmcont1hostname = rmcont1.split('.')
+   #rmcont2hostname = rmcont2.split('.')
+   #rmcont3hostname = rmcont3.split('.')
+   #cluster_nodes << "rabbit@"+rmcont1hostname[0]
+   #cluster_nodes << "rabbit@"+rmcont2hostname[0]
+   #cluster_nodes << "rabbit@"+rmcont3hostname[0]
+   #node.default['rabbitmq']['cluster_disk_nodes'] = cluster_nodes
+  ##End of cluster cluster address config
 
   log "stopping service[#{node['rabbitmq']['service_name']}] to change erlang_cookie" do
     level :info
@@ -196,6 +196,8 @@ if node['rabbitmq']['cluster'] && (node['rabbitmq']['erlang_cookie'] != existing
     notifies :start, "service[#{node['rabbitmq']['service_name']}]", :immediately
     notifies :run, "execute[reset-node]", :immediately
   end
+  
+  
 
   # Need to reset for clustering #
   execute "reset-node" do
